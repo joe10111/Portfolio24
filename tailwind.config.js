1037d7aec6b403fb6f0c6a7c27b0810cffcff6e1
joe <centeno.joe@gmail.com>
2024-05-03 11:43:44 -0700

    Moblie Freindly!

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': '640px', // default, adjust if necessary
+        'md': '768px', // default, adjust if necessary
+        'lg': '1024px', // default, adjust if necessary
+        'xl': '1280px', // default, adjust if necessary
+        '2xl': '1536px', // default, adjust if necessary
+      },
+      width: {
+        'logo-sm': '5rem', // Example sizes
+        'logo-md': '6rem',
+        'logo-lg': '8rem',
+      }
+    },
   },
   plugins: [],
 }
